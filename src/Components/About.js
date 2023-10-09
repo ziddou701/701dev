67781c349f35aee0ed7fd86f2ca2628804eb467a
ziddou701 <ousmane.z.konate@hotmail.com>
2023-10-08 21:10:31 -0400

    nothing much

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,6 @@
 const About = () => {
     return (
-        <div className="relative w-full min-h-screen bg-[#d0d0d0] mx-auto snap-start" id="About">
+        <div className="relative w-full min-h-screen bg-[#d0d0d0] mx-auto snap-start" >
 
         </div>
     );
